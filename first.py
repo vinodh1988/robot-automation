f82513c461856783d87d8597af660fce3727ee35
vinodh1988 <k.c.vinodh@hotmail.com>
2024-06-17 20:21:13 +0530

    first-commit

---
+++ first.py
@@ -0,0 +1,13 @@
+print("First ever python program")
+
+#this is comment
+
+# this is my first ever python program
+# in python we dont need to specify data types for variables
+
+a=90 # you can also string, float, boolean, array, object, function
+b="Rahul"
+c=90.543
+d=True
+
+print(a,b,c,d)
