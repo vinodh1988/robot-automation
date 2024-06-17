d4b9942e6ab3f4188829390a39c82d2edb2894e2
vinodh1988 <k.c.vinodh@hotmail.com>
2024-06-17 20:51:02 +0530

    ifelse

---
+++ interactive.py
@@ -0,0 +1,7 @@
+save=input("Do you want to save (Y/N)?:")
+if save=="Y":
+    print("Your work is being saved")
+elif save=="N":
+    print("you did not chose to save it")
+else:
+    print("The option you have chosen is wrong")
