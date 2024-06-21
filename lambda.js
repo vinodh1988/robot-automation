f4676d5c57f104940d247b8ea23f0fbcdf10293d
vinodh1988 <k.c.vinodh@hotmail.com>
2024-06-21 20:38:41 +0530

    new-commit

---
+++ lambda.js
@@ -0,0 +1,9 @@
+add=function(x,y) {
+    return x+y
+} //anonymous function
+
+console.log(add(34,35))
+
+sum = (x,y) => x+y //arrow function
+
+console.log(sum(34,35))
--- untilday3/access.py
+++ untilday3/access.py
@@ -1,5 +1,5 @@
-import config
-from config import awsconfig,dbconfig
+import myops.config as config
+from myops.config import awsconfig,dbconfig
 
 config.sayHello()
 print(config.dbconfig["dburl"])
